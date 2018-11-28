5cccda91063ad23a0222d9175f0f17f956eddc10
Adrian Polak <adrian.polak@capgemini.com>
2018-11-28 11:06:52 +0100

    Offermanagement mock tests for special

--- samples/core/src/test/java/io/oasp/gastronomy/restaurant/offermanagement/logic/impl/OffermanagementImplTest.java
+++ samples/core/src/test/java/io/oasp/gastronomy/restaurant/offermanagement/logic/impl/OffermanagementImplTest.java
@@ -1,7 +1,11 @@
 package io.oasp.gastronomy.restaurant.offermanagement.logic.impl;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,10 +15,15 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
 import io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api.OfferEntity;
+import io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api.SpecialEntity;
 import io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api.dao.OfferDao;
+import io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api.dao.SpecialDao;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.OfferCto;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.OfferEto;
+import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.SpecialEto;
+import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.SpecialSearchCriteriaTo;
 import io.oasp.module.beanmapping.common.api.BeanMapper;
 import io.oasp.module.test.common.base.ModuleTest;
 
@@ -41,6 +50,9 @@ public class OffermanagementImplTest extends ModuleTest {
   private OfferDao offerDao;
 
   @Mock
+  private SpecialDao specialDao;
+
+  @Mock
   private BeanMapper beanMapper;
 
   /**
@@ -54,6 +66,7 @@ public class OffermanagementImplTest extends ModuleTest {
     this.offerManagementImpl = new OffermanagementImpl();
     this.offerManagementImpl.setOfferDao(this.offerDao);
     this.offerManagementImpl.setBeanMapper(this.beanMapper);
+    this.offerManagementImpl.setSpecialDao(this.specialDao);
   }
 
   /**
@@ -65,6 +78,7 @@ public class OffermanagementImplTest extends ModuleTest {
     this.beanMapper = null;
     this.offerDao = null;
     this.offerManagementImpl = null;
+    this.specialDao = null;
   }
 
   /**
@@ -113,4 +127,111 @@ public class OffermanagementImplTest extends ModuleTest {
 
   }
 
+  /**
+   * This method tests the execution of the findOffer method belonging to the {@link OffermanagementImpl} class. It
+   * includes the search for Special Offers
+   */
+  @Test
+  public void findOfferWithSpecial() {
+
+    // given
+    Money testSpecialprice = new Money(2.22);
+    OfferEntity offerEntity = mock(OfferEntity.class);
+    OfferEto offerEto = new OfferEto();
+    offerEto.setSpecialPrice(testSpecialprice);
+
+    SpecialEntity specialEntity = new SpecialEntity();
+    specialEntity.setSpecialPrice(testSpecialprice);
+
+    when(this.offerDao.findOne(ID)).thenReturn(offerEntity);
+    when(this.specialDao.getFirstSpecialForSearchCriteria(any(SpecialSearchCriteriaTo.class)))
+        .thenReturn(specialEntity);
+    when(this.beanMapper.map(offerEntity, OfferEto.class)).thenReturn(offerEto);
+
+    // when
+    OfferEto responseOfferEto = this.offerManagementImpl.findOffer(ID);
+
+    // then
+    assertThat(responseOfferEto).isNotNull();
+    assertThat(responseOfferEto).isEqualTo(offerEto);
+    assertThat(responseOfferEto.getSpecialPrice()).isEqualTo(testSpecialprice);
+  }
+
+  /**
+   * This method tests if a special offer can be saved.
+   */
+  @Test
+  public void shouldSaveSpecial() {
+
+    // given
+    SpecialEto specialEto = new SpecialEto();
+    SpecialEntity specialEntity = new SpecialEntity();
+
+    when(this.specialDao.save(specialEntity)).thenReturn(specialEntity);
+    when(this.beanMapper.map(specialEntity, SpecialEto.class)).thenReturn(specialEto);
+    when(this.beanMapper.map(specialEto, SpecialEntity.class)).thenReturn(specialEntity);
+
+    // when
+    SpecialEto responseSpecialEto = this.offerManagementImpl.saveSpecial(specialEto);
+
+    // then
+    assertThat(responseSpecialEto).isNotNull();
+    assertThat(responseSpecialEto).isEqualTo(specialEto);
+  }
+
+  /**
+   * This method tests loading of a Special.
+   */
+  @Test
+  public void shouldLoadSpecial() {
+
+    // given
+    SpecialEto specialEto = new SpecialEto();
+    specialEto.setId(ID);
+    SpecialEntity specialEntity = new SpecialEntity();
+
+    when(this.specialDao.findOne(ID)).thenReturn(specialEntity);
+    when(this.beanMapper.map(specialEntity, SpecialEto.class)).thenReturn(specialEto);
+    when(this.beanMapper.map(specialEto, SpecialEntity.class)).thenReturn(specialEntity);
+
+    // when
+    SpecialEto responseSpecialEto = this.offerManagementImpl.findSpecialEto(ID);
+
+    // then
+    assertThat(responseSpecialEto).isNotNull();
+    assertThat(responseSpecialEto).isEqualTo(specialEto);
+    assertThat(responseSpecialEto.getId()).isEqualTo(ID);
+  }
+
+  /**
+   * This method tests loading of a 2 Special.
+   */
+  @Test
+  public void shouldLoadAllSpecial() {
+
+    // given
+    SpecialEto firstSpecialEto = new SpecialEto();
+    SpecialEto secondSpecialEto = new SpecialEto();
+    List<SpecialEto> specialEtos = new ArrayList<>(2);
+    specialEtos.add(firstSpecialEto);
+    specialEtos.add(secondSpecialEto);
+
+    List<SpecialEntity> specialEntities = new ArrayList<>(2);
+    SpecialEntity firstSpecialEntity = new SpecialEntity();
+    SpecialEntity secondSpecialEntity = new SpecialEntity();
+    specialEntities.add(firstSpecialEntity);
+    specialEntities.add(secondSpecialEntity);
+
+    when(this.specialDao.findAll()).thenReturn(specialEntities);
+    when(this.beanMapper.mapList(specialEntities, SpecialEto.class)).thenReturn(specialEtos);
+
+    // when
+    List<SpecialEto> responseSpecialEtos = this.offerManagementImpl.findAllSpecials();
+
+    // then
+    assertThat(responseSpecialEtos).isNotNull();
+    assertThat(responseSpecialEtos.size()).isEqualTo(2);
+    assertThat(responseSpecialEtos.get(0)).isEqualTo(firstSpecialEto);
+    assertThat(responseSpecialEtos.get(1)).isEqualTo(secondSpecialEto);
+  }
 }
